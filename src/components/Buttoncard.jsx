b9912bd1a12bd34c03524412a1d2313a82877deb
hevmag <90092645+hevmag@users.noreply.github.com>
2022-11-24 15:10:58 +0000

    add light up functionality

--- src/components/Buttoncard.jsx
+++ src/components/Buttoncard.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const Buttoncard = ({ soundButton }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   const audio = new Audio(soundButton.link);
+  const handleButtonPress = () => {
+    setIsPlaying(true)
+    audio.play()
+    setTimeout(() => {
+      setIsPlaying(false)
+    }, 300)
+  }
 
   return (
-    <div className='button--card' onClick={() => audio.play()}>{soundButton.id}</div>
+    <div className={isPlaying ? 'button--card--active' : 'button--card'} onClick={handleButtonPress}>{soundButton.id}</div>
   )
 }
