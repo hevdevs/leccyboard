cd59dfed6fa5ce5de999824f094ba130ab07e7fa
hevmag <90092645+hevmag@users.noreply.github.com>
2022-11-24 15:12:00 +0000

    add order property

--- src/components/Soundboard.jsx
+++ src/components/Soundboard.jsx
@@ -1,79 +1,77 @@
 import React from 'react';
 import { Buttoncard } from './Buttoncard';
+import StartButton from './StartButton';
 
 export const Soundboard = () => {
-	// https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3
-	// https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3
+
 	const soundButtons = [
 		{
 			id: 1,
 			description: '',
-			link: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+			link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
 			order: null,
 		},
 		{
 			id: 2,
 			description: '',
 			link: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+			order: null,
 		},
 		{
 			id: 3,
 			description: '',
 			link: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+			order: null,
 		},
 		{
 			id: 4,
 			description: '',
-			link: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+			link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+			order: null,
 		},
 		{
 			id: 5,
 			description: '',
 			link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+			order: null,
 		},
 		{
 			id: 6,
 			description: '',
 			link: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+			order: null,
 		},
 		{
 			id: 7,
 			description: '',
-			link: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+			link: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+			order: null,
 		},
 		{
 			id: 8,
 			description: '',
 			link: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+			order: null,
 		},
 		{
 			id: 9,
 			description: '',
 			link: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+			order: null,
 		},
 	];
 
 	return (
-		<div className="sound--board">
+		<>
+			<div className="sound--board">
 				{soundButtons.map((soundButton) => (
-					<Buttoncard key={soundButton.id} soundButton={soundButton} />
+					<Buttoncard
+						key={soundButton.id}
+						soundButton={soundButton}
+					/>
 				))}
-		</div>
+			</div>
+			<StartButton />
+		</>
 	);
 };
