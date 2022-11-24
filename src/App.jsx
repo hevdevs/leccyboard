1c70df40689cf55728012a3714bfb58eaaf2b271
hevmag <90092645+hevmag@users.noreply.github.com>
2022-11-24 15:10:05 +0000

    remove unneccessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import { Header } from './components/Header'
 import { Soundboard } from './components/Soundboard'
+
 import './style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Header />
-      <Soundboard/>
+      <Soundboard />
     </div>
   )
 }
