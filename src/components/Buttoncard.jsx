6329cade0d6f73ad1f8468f7f41c5520a8473a83
hevmag <90092645+hevmag@users.noreply.github.com>
2022-11-25 09:14:05 +0000

    add lose/win logic for button press

--- src/components/Buttoncard.jsx
+++ src/components/Buttoncard.jsx
@@ -1,27 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-export const Buttoncard = ({ soundButton, playerSequence, setPlayerSequence, simonSequence }) => {
-  const [isPlaying, setIsPlaying] = useState(false);
+export const Buttoncard = ({
+	soundButton,
+	setPlayerSequence,
+	playerSequence,
+	setGameStarted,
+	simonSequence,
+	setGamesWon,
+	setSequencePos,
+  sequencePos
+}) => {
+	const [isPlaying, setIsPlaying] = useState(false);
 
-  const audio = new Audio(soundButton.link);
-  
-  const handleButtonPress = () => {
-    setIsPlaying(true)
-    if (soundButton.id !== simonSequence[soundButton.id]) {
-      console.log('LOSERR!')
+	const audio = new Audio(soundButton.link);
+
+	const handleButtonPress = () => {
+		setIsPlaying(true);
+		if (simonSequence[sequencePos] !== soundButton.id) {
+			//lose scenario
+			audio.play();
+			setPlayerSequence([]);
+			setSequencePos(0);
+			setTimeout(() => {
+				setIsPlaying(false);
+			}, 300);
+			setGameStarted(false);
+		} else if (
+			(simonSequence[sequencePos] === soundButton.id &&
+				simonSequence.length) ===
+			playerSequence.length + 1
+    ) {
+      //win scenario
+			audio.play();
+			setTimeout(() => {
+				setIsPlaying(false);
+			}, 300);
+			setGamesWon((currVal) => ++currVal);
+			setSequencePos(0);
+			setPlayerSequence([]);
+			setGameStarted(false);
     } else {
-      setPlayerSequence((currSequence) => {
-        return [...currSequence, soundButton.id]
-      });
-      audio.play()
-      setTimeout(() => {
-        setIsPlaying(false)
-      }, 300)
-    }
-  
-  }
+      //correct button press, sequence incomplete
+			setSequencePos((currVal) => {
+				return ++currVal;
+			});
+			setPlayerSequence((currSequence) => {
+				return [...currSequence, soundButton.id];
+			});
+			audio.play();
+			setTimeout(() => {
+				setIsPlaying(false);
+			}, 300);
+		}
+	};
 
-  return (
-    <div id={soundButton.id} className={isPlaying ? 'button--card--active' : 'button--card'} onClick={handleButtonPress}></div>
-  )
-}
+	return (
+		<div
+			id={soundButton.id}
+			className={isPlaying ? 'button--card--active' : 'button--card'}
+			onClick={handleButtonPress}
+		></div>
+	);
+};
